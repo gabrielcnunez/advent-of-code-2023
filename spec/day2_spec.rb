735a09783c8f543bd0dfc7b791b76848a1dc2520
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-06 16:19:31 -0700

    Pass testing for day two challenge

--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require './lib/day1.rb'
+require './lib/day2.rb'
 
 describe 'Day 2: Cube Conundrum' do
   describe "Is This Game Possible" do
@@ -19,7 +19,7 @@ describe 'Day 2: Cube Conundrum' do
       Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
       Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
       Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
-      expect(id_sum(games)).to eq(8)
+      expect(ids_sum(games)).to eq(8)
     end
   end
 end
