217293186e06e3eb3566584abe560fd81fb986eb
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-08 16:13:57 -0700

    Create test for day 3 challenge

---
+++ spec/day3_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/day3.rb'
+
+describe 'Day 3: Gear Ratios' do
+  describe "Sum of All Part Numbers" do
+    it "takes string of engine schematic, returns sum of all part numbers" do
+      expect(part_numbers_sum('467..114..
+      ...*......
+      ..35..633.
+      ......#...
+      617*......
+      .....+.58.
+      ..592.....
+      ......755.
+      ...$.*....
+      .664.598..')).to eq(4361)
+    end
+  end
+end
