3db79cad302db5e5f5d51ffa040508b3155b4452
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-10-09 17:32:16 -0700

    Create test for day three part two challenge

--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -16,4 +16,19 @@ describe 'Day 3: Gear Ratios' do
       .664.598..')).to eq(4361)
     end
   end
+
+  describe "Sum of All Gear Ratios" do
+    it "takes string of engine schematic, returns sum of all gear ratios" do
+      expect(gear_ratios_sum('467..114..
+      ...*......
+      ..35..633.
+      ......#...
+      617*......
+      .....+.58.
+      ..592.....
+      ......755.
+      ...$.*....
+      .664.598..')).to eq(467835)
+    end
+  end
 end
